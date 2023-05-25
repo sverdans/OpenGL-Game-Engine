6685bb8cffe32223ccc92c6e3ea2de65a607d718
sverdans <dev.sverdans@gmail.com>
2023-05-26 01:11:14 +0300

    cmakelist update

--- linux-preinstall.sh
+++ linux-preinstall.sh
@@ -1,7 +1,2 @@
 sudo apt-get install libxss-dev libxxf86vm-dev libxkbfile-dev libxv-dev
-sudo apt install libxrandr-dev 
-sudo apt install libxinerama-dev
-sudo apt install libxcursor-dev
-sudo apt install libxi-dev
-#sudo apt-get install libmesa-dev
-sudo apt install libgl-dev
+sudo apt install libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libgl-dev
